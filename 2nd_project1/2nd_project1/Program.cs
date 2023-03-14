d37a45065584bde8f89b00ae976e03403991372f
taewook <xodnr2019@naver.com>
2023-03-14 14:25:15 +0900

    2nd_project1

---
+++ 2nd_project1/2nd_project1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2nd_project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
