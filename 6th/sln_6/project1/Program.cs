9bfe79f8a860244b70d22b4ca8e9ae645a038fdc
Github-AppData <77886661+Github-AppData@users.noreply.github.com>
2023-04-11 16:37:48 +0900

    5_6주차

---
+++ 6th/sln_6/project1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] scores = { 75, 12, 100, 98, 27 };
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                int sum;
+
+                Console.Write(scores[i]);
+                sum =+ scores[i];
+            }
+            int average = sum / scores.Length;
+            Console.WriteLine(average);
+            Console.WriteLine("average score");*/
+            /*
+                        int input = 1;
+                        for (int i = 1; i <= 10; i++)
+                        {
+                            input *= i;
+                        }
+                        Console.WriteLine(input);*/
+            /*
+                        int[] intArray = { 10, 20, 30, 40, 50, 60, 70 };
+                        for (int i = intArray.Length; i >= 0; i--)
+                        {
+                            Console.WriteLine(intArray[i]);
+                        }*/
+            /*
+                        // foreach 반복문과 배열
+                        string[] array = { "사과", "용과", "포도", "딸기", "키위"};
+                        foreach (var item in array)
+                        {
+                            Console.WriteLine(item);
+                        }*/
+
+            // while 반복문
+            /*int i = 0;
+            int[] intArray = { 52,1251,112,922,9022,187,1236};
+
+            while (i < intArray.Length)
+            {
+                Console.WriteLine("i번째 출력 : " + intArray[i]);
+                i++;
+            }*/
+            /*
+                        int[] intArray = { 52, 1251, 112, 922, 9022, 187, 1236 };
+
+                        for (int i = 0; i < intArray.Length; i++)
+                        {
+                            Console.WriteLine("i번째 출력 : " + intArray[i]);
+                        }*/
+
+            /*// do while
+            string input;
+            do
+            {
+                Console.WriteLine("입력 (exit를 입력하면 종료) : ");
+                input = Console.ReadLine();
+            } while (input != "exit");
+            Console.WriteLine("나갔습니다.");
+*/
+            int[] scores = { 10, 20, 30, 40, 50 };
+            /*Console.WriteLine(scores[-1]);*/
+        }
+    }
+}
