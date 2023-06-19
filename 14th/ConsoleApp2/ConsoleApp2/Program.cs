594b21039362d19e918d1831339a4fbb5a137be1
TaeWook <xodnr2019@naver.com>
2023-06-19 23:25:06 +0900

    14th

---
+++ 14th/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		
+		class Product
+		{
+			public string Name { get; set; }
+			public int Price { get; set; }
+		}
+
+		// 델리게이트 실습 - 1. 정렬 
+		// 가격으로 정렬 하겠다.
+		static int SortWithPrice(Product a, Product b)
+		{
+			// a와 b 상품의 가격을 비교를 해서 리턴을 하겠다.
+			// CompareTo는 메서드가 아니라 딜리게이터
+			return a.Price.CompareTo(b.Price); 
+		}
+
+
+		static void Main(string[] args)
+		{
+			// List를 생성 
+			List<Product> products = new List<Product>() 
+			{ 
+				new Product() {Name = "감자", Price = 5000},
+				new Product() {Name = "감자2", Price = 1000},
+				new Product() {Name = "감자3", Price = 2000},
+				new Product() {Name = "감자4", Price = 3000}
+			};
+
+			// 정렬
+			products.Sort(SortWithPrice);
+
+			// 출력
+			foreach (var item in products)
+			{
+				Console.WriteLine(item.Name + " : " + item.Price);
+			}
+		}
+	}
+}
---
+++ 14th/ConsoleApp2/UnnamedDelegate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnnamedDelegate
+{
+	class Program
+	{
+		
+		class Product
+		{
+			public string Name { get; set; }
+			public int Price { get; set; }
+		}
+
+		
+
+		static void Main(string[] args)
+		{
+			// List를 생성 
+			List<Product> products = new List<Product>()
+			{
+				new Product() {Name = "감자", Price = 5000},
+				new Product() {Name = "감자2", Price = 1000},
+				new Product() {Name = "감자3", Price = 2000},
+				new Product() {Name = "감자4", Price = 3000}
+			};
+
+			// 델리게이터를 좀 더 편하게 사용할 때는 람다.
+			// 델리게이터를 간단하고 편리하게, 매개변수의 자료형을 지정할 필요 없다.
+
+			// 무명 델리게이트로 정렬 방법 정의
+			// 무명 이지만, 메서드 형식을 갖추고 있다.
+			/*products.Sort(
+				delegate (Product a, Product b)
+				{
+					return a.Price.CompareTo(b.Price);
+				}
+				);*/
+
+			// Sort() 메서드의 매개 변수로 람다를 지정
+			products.Sort((a, b) => a.Price.CompareTo(b.Price));
+
+
+			// 출력
+			foreach (var item in products)
+			{
+				Console.WriteLine(item.Name + " : " + item.Price);
+			}
+		}
+	}
+}
